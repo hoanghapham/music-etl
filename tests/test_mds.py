7bcf9e621921ab5b504f08856f40adc2fb1def37
hoanghapham <hoangha.20021992@gmail.com>
2023-01-26 16:54:38 +0700

    update tests

--- tests/test_mds.py
+++ tests/test_mds.py
@@ -2,41 +2,47 @@
 from pytest import fixture
 from src.mds import SongsExtractor
 
-def test_song_extractor():
+@fixture
+def songs_extractor():
     songs_extractor = SongsExtractor()
-    song_01 = songs_extractor.extract_one("tests/fixtures/mds/input/song_01.h5")
-    songs = songs_extractor.extract_many("tests/fixtures/mds/input/*.h5")
+    return songs_extractor
 
-    # %%
+class TestSongsExtractor():
 
-    output_song_01 = {
-        'id': 'SOCIWDW12A8C13D406',
-        'title': 'Soul Deep',
-        'release': 'Dimensions',
-        'genre': '',
-        'artist_id': 'ARMJAGH1187FB546F3',
-        'year': 1969
-    }
+    def test_extract_one(self, songs_extractor):
+        song_01 = songs_extractor.extract_one("tests/fixtures/mds/input/song_01.h5")
 
-    output_songs = [
-        {
+        output_song_01 = {
             'id': 'SOCIWDW12A8C13D406',
             'title': 'Soul Deep',
             'release': 'Dimensions',
             'genre': '',
             'artist_id': 'ARMJAGH1187FB546F3',
             'year': 1969
-        },
-        {
-            'id': 'SONHOTT12A8C13493C',
-            'title': 'Something Girls',
-            'release': 'Friend Or Foe',
-            'genre': '',
-            'artist_id': 'AR7G5I41187FB4CE6C',
-            'year': 1982
         }
-    ]
 
-    assert song_01 == output_song_01
-    assert all([i in output_songs for i in songs])
-    # %%
+        assert song_01 == output_song_01
+
+    def test_extract_many(self, songs_extractor):
+        songs = songs_extractor.extract_many("tests/fixtures/mds/input/*.h5")
+        output_songs = [
+            {
+                'id': 'SOCIWDW12A8C13D406',
+                'title': 'Soul Deep',
+                'release': 'Dimensions',
+                'genre': '',
+                'artist_id': 'ARMJAGH1187FB546F3',
+                'year': 1969
+            },
+            {
+                'id': 'SONHOTT12A8C13493C',
+                'title': 'Something Girls',
+                'release': 'Friend Or Foe',
+                'genre': '',
+                'artist_id': 'AR7G5I41187FB4CE6C',
+                'year': 1982
+            }
+        ]
+        assert all([i in output_songs for i in songs])
+
+
