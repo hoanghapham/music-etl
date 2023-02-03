d2632990bc9ad86c96f9fa95be70aefd61936be8
hoanghapham <hoangha.20021992@gmail.com>
2023-02-03 12:44:20 +0700

    add argument parser

--- etl/flows/extract_msd_data.py
+++ etl/flows/extract_msd_data.py
@@ -7,54 +7,68 @@ from src.msd import SongExtractor, ArtistExtractor
 from src.aws.s3 import S3Client
 from src.utils.custom_logger import init_logger
 
-# Set up
-p = Path(__file__).with_name('config.cfg')
-config = ConfigParser()
-config.read(p)
+import argparse
 
-bucket = config['S3']['BUCKET']
-data_dir = config['DATA']['DATA_DIR']
-input_dir = config['DATA']['MSD_INPUT_DIR']
-output_dir = f"{data_dir}/msd/"
+def main(search_dirs):
 
-logger = init_logger(Path(__file__).name)
+    # Set up
+    p = Path(__file__).with_name('config.cfg')
+    config = ConfigParser()
+    config.read(p)
 
-# Prepare client
-client = S3Client(
-    region_name='us-west-2', 
-    aws_access_key_id=config['AWS']['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key=config['AWS']['AWS_SECRET_ACCESS_KEY'],
-    aws_session_token=config['AWS']['AWS_SESSION_TOKEN'],
-)
+    bucket = config['S3']['BUCKET']
+    data_dir = config['DATA']['DATA_DIR']
+    input_dir = config['DATA']['MSD_INPUT_DIR']
+    output_dir = f"{data_dir}/msd/"
+
+    logger = init_logger(Path(__file__).name)
+
+    # Prepare client
+    client = S3Client(
+        region_name='us-west-2', 
+        aws_access_key_id=config['AWS']['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key=config['AWS']['AWS_SECRET_ACCESS_KEY'],
+        aws_session_token=config['AWS']['AWS_SESSION_TOKEN'],
+    )
 
 
-#%%
-song_extractor = SongExtractor()
-artists_extractor = ArtistExtractor()
+    #%%
+    song_extractor = SongExtractor()
+    artists_extractor = ArtistExtractor()
 
-patterns = []
+    patterns = []
 
-for char1 in string.ascii_uppercase:
-    for char2 in string.ascii_uppercase:
-        patterns.append(f"{char1}/{char2}")
 
-for pattern in patterns:
-    search_path         = f"{input_dir}/{pattern}/**/*.h5"
-    songs_output_dir   = f"{output_dir}/songs/{pattern}.json"
-    artists_output_dir = f"{output_dir}/artists/{pattern}.json"
-    songs_remote_path   = f"msd/songs/{pattern}.json"
-    artists_remote_path = f"msd/artists/{pattern}.json"
+    for char1 in search_dirs:
+        for char2 in string.ascii_uppercase:
+            patterns.append(f"{char1}/{char2}")
 
-    logger.info(f"Processing {search_path}")
+    for pattern in patterns:
+        search_path         = f"{input_dir}/{pattern}/**/*.h5"
+        songs_output_dir   = f"{output_dir}/songs/{pattern}.json"
+        artists_output_dir = f"{output_dir}/artists/{pattern}.json"
+        songs_remote_path   = f"msd/songs/{pattern}.json"
+        artists_remote_path = f"msd/artists/{pattern}.json"
 
-    songs = song_extractor.extract_many_files(search_path)
-    artists = artists_extractor.extract_many_files(search_path)
+        logger.info(f"Processing {search_path}")
 
-    if songs == []:
-        continue
-    else:
-        song_extractor.output_json(songs, songs_output_dir)
-        client.upload_file(songs_output_dir, bucket, songs_remote_path)
+        songs = song_extractor.extract_many_files(search_path)
+        artists = artists_extractor.extract_many_files(search_path)
 
-        artists_extractor.output_json(artists, artists_output_dir)
-        client.upload_file(artists_output_dir, bucket, artists_remote_path)
+        if songs == []:
+            continue
+        else:
+            song_extractor.output_json(songs, songs_output_dir)
+            client.upload_file(songs_output_dir, bucket, songs_remote_path)
+
+            artists_extractor.output_json(artists, artists_output_dir)
+            client.upload_file(artists_output_dir, bucket, artists_remote_path)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--search_dirs", default="A")
+
+    args = parser.parse_args()
+
+    main(args.search_dirs)
